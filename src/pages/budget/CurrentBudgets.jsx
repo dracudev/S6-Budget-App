0480f2a4822c57e02fb605afe57d03a656130e82
dracudev <andreujavier99@gmail.com>
2024-11-26 18:31:20 +0100

    finising current budget styles

--- src/pages/budget/CurrentBudgets.jsx
+++ src/pages/budget/CurrentBudgets.jsx
@@ -4,11 +4,9 @@ import { CustomCard } from "../../components/CustomCard";
 
 export function CurrentBudgets() {
   const { submittedData } = useBudget();
-  if (!submittedData) {
-    return <div>No budgets submitted yet.</div>;
-  }
+
   return (
-    <div className=" data-container">
+    <div className=" budget-container">
       <hr className="mt-3 mb-5 dotted-hr" />
       <div className="d-flex">
         <h2 className="ms-3">
@@ -19,32 +17,71 @@ export function CurrentBudgets() {
         </div>
       </div>
 
-      <div className="mt-5 d-flex align-items-center justify-content-center">
-        {submittedData.length === 0 && <h4 className="">No current budgets</h4>}
+      <div className="mt-5 d-flex flex-column">
+        {submittedData.length === 0 && <h4 className="d-flex align-items-center justify-content-center">No current budgets</h4>}
 
         {submittedData.map((data, index) => (
-          <CustomCard key={index} className="container">
-            <h3>Submitted Information</h3>
-            <p>
-              <strong>Name:</strong> {data.text}
-            </p>
-            <p>
-              <strong>Telephone:</strong> {data.tel}
-            </p>
-            <p>
-              <strong>Email:</strong> {data.email}
-            </p>
-            <p>
-              <strong>Budget:</strong> {data.budget}€
-            </p>
-            <p>
-              <strong>Elements:</strong> Pages: {data.elements.pages},
-              Languages: {data.elements.languages}
-            </p>
-            <p>
-              <strong>Checked Items:</strong>{" "}
-              {JSON.stringify(data.checkedItems)}
-            </p>
+          <CustomCard key={index} className="container mb-3  ">
+            <div className="card-items">
+              <div className="card-section">
+                <div>
+                  <h2>
+                    <strong>{data.text}</strong>
+                  </h2>
+                  <p className="lead m-0">{data.email}</p>
+                  <p className="lead m-0">{data.tel}</p>
+                </div>
+              </div>
+
+              <div className="card-section">
+                <div>
+                  <p className="m-0">
+                    <strong>Contracted services:</strong>
+                  </p>
+                  <ul className="mt-2">
+                    {Object.keys(data.checkedItems).map((item) => {
+                      if (data.checkedItems[item]) {
+                        if (item === "web") {
+                          const pages =
+                            data.elements.pages > 0
+                              ? `${data.elements.pages} pages`
+                              : "";
+                          const languages =
+                            data.elements.languages > 0
+                              ? `${data.elements.languages} languages`
+                              : "";
+                          const separator = pages && languages ? ", " : "";
+                          return (
+                            <li key={item}>
+                              Web{" "}
+                              {pages || languages
+                                ? `(${pages}${separator}${languages})`
+                                : ""}
+                            </li>
+                          );
+                        } else {
+                          return (
+                            <li key={item}>
+                              {item.charAt(0).toUpperCase() + item.slice(1)}
+                            </li>
+                          );
+                        }
+                      }
+                      return null;
+                    })}
+                  </ul>
+                </div>
+              </div>
+
+              <div className="card-section">
+                <p className="lead m-0">
+                  <strong>Total:</strong>
+                </p>
+                <h5>
+                  <strong className="fs-1">{data.budget}</strong>€
+                </h5>
+              </div>
+            </div>
           </CustomCard>
         ))}
       </div>
