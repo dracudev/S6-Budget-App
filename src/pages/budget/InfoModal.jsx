f61ea2f47511a0bff744ab886e08834a6d24d76d
dracudev <andreujavier99@gmail.com>
2024-11-27 10:34:50 +0100

    LV2- EX6 (Help Pop-ups)

---
+++ src/pages/budget/InfoModal.jsx
@@ -0,0 +1,22 @@
+import Modal from "react-bootstrap/Modal";
+import PropTypes from "prop-types";
+
+export function InfoModal({ show, onHide, title, description }) {
+  return (
+    <Modal show={show} onHide={onHide} size="lg" centered> 
+      <Modal.Body className="d-flex align-items-center justify-content-center flex-column">
+        <div className="d-flex align-items-center justify-content-center flex-column m-5">
+        <h3 className="py-3"><strong>{title}</strong></h3>
+        <p className="py-3">{description}</p>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+InfoModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
--- src/pages/budget/WebElements.jsx
+++ src/pages/budget/WebElements.jsx
@@ -1,7 +1,8 @@
 import "./WebElements.css";
 import PropTypes from "prop-types";
-import { CiCircleMinus } from "react-icons/ci";
-import { CiCirclePlus } from "react-icons/ci";
+import { useState } from "react";
+import { CiCircleInfo, CiCircleMinus, CiCirclePlus } from "react-icons/ci";
+import { InfoModal } from "./InfoModal";
 
 export function WebElements({
   elements,
@@ -9,9 +10,33 @@ export function WebElements({
   handleClickAdd,
   handleClickRest,
 }) {
+  const [modalShow, setModalShow] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    title: "",
+    description: "",
+  });
+
+  function handleModal(type) {
+    if (type === "pages") {
+      setModalContent({
+        title: "Number of pages",
+        description: "Add the number of pages ​​your project will have. The cost of each page is 30€.",
+      });
+    } else if (type === "languages") {
+      setModalContent({
+        title: "Number of languages",
+        description: "Add the number of languages ​​your project will have. The cost of each language is 30€.",
+      });
+    }
+    setModalShow(true);
+  };
+
   return (
     <div className="number-container">
       <div className="number-input">
+        <button onClick={() => handleModal("pages")} className="">
+          <CiCircleInfo />
+        </button>
         <p className="pages">Pages</p>
         <div className="d-flex">
           <button onClick={() => handleClickRest("pages")}>
@@ -30,6 +55,9 @@ export function WebElements({
         </div>
       </div>
       <div className="number-input">
+        <button onClick={() => handleModal("languages")}>
+          <CiCircleInfo />
+        </button>
         <p>Languages</p>
         <div className="d-flex">
           <button type="button" onClick={() => handleClickRest("languages")}>
@@ -47,6 +75,12 @@ export function WebElements({
           </button>
         </div>
       </div>
+      <InfoModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        title={modalContent.title}
+        description={modalContent.description}
+      ></InfoModal>
     </div>
   );
 }
